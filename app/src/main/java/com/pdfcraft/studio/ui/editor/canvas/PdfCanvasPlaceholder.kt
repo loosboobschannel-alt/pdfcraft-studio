6c8ce6e54970669aad7df8cd1ac8031d4cdc01f0
Faisal Shahzad <loosboobschannel@gmail.com>
2026-08-17 02:41:23 +0500

    Fix missing sp import and clean TextStyle in PdfCanvasPlaceholder

--- app/src/main/java/com/pdfcraft/studio/ui/editor/canvas/PdfCanvasPlaceholder.kt
+++ app/src/main/java/com/pdfcraft/studio/ui/editor/canvas/PdfCanvasPlaceholder.kt
@@ -79,6 +79,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pdfcraft.studio.R
 import com.pdfcraft.studio.core.text.AppFont
 import com.pdfcraft.studio.core.text.FontCatalog
