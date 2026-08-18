da97fa079e5f83ee76250c76a266f94a7eb0f00b
Faisal Shahzad <loosboobschannel@gmail.com>
2026-08-18 23:40:09 +0500

    Add 5 KB preset before 10 KB in Import Images

--- app/src/main/java/com/pdfcraft/studio/core/image/ImageSizeOption.kt
+++ app/src/main/java/com/pdfcraft/studio/core/image/ImageSizeOption.kt
@@ -20,6 +20,6 @@ sealed class ImageSizeOption {
         }
 
     companion object {
-        val presetsKb = listOf(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 250, 500, 750, 1000)
+        val presetsKb = listOf(5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 250, 500, 750, 1000)
     }
 }
