471935328dd0aec13e85c5d59ee35fd7470adb41
Faisal Shahzad <loosboobschannel@gmail.com>
2026-08-22 08:48:48 +0500

    Fix Modifier import: androidx.compose.ui.Modifier not modifier.Modifier

--- app/src/main/java/com/pdfcraft/studio/ui/editor/EditorToolbar.kt
+++ app/src/main/java/com/pdfcraft/studio/ui/editor/EditorToolbar.kt
@@ -39,7 +39,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier.Modifier
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.stringResource
